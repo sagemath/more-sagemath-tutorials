35e03f1774f9656fbe60014db491fefdddd19dd7
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-07-25 17:10:45 +0200

    Disable building jupyter notebook with rst2ipynb on RTD

--- conf.py
+++ conf.py
@@ -299,7 +299,6 @@ def rtd_finalize(app, exception):
     import subprocess
     if exception is None:
         subprocess.call("make jupyter", shell=True)
-        subprocess.call("make ipynb", shell=True)
         subprocess.call("make media", shell=True)
 
 def setup(app):
