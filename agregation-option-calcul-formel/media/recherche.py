d281987ccb5b0a7823e52917e51e8f05551a5736
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-11-21 07:58:57 +0100

    Reformatage code

--- agregation-option-calcul-formel/media/recherche.py
+++ agregation-option-calcul-formel/media/recherche.py
@@ -3,18 +3,19 @@
 
 def recherche(liste,val):
     """
-    Renvoie ``true`` si ``val`` est present dans ``liste``, False sinon.
+    Détermine si ``val`` est présent dans ``liste``.
 
     INPUT:
 
     - ``liste`` -- une liste.
 
+    OUTPUT: un booléen
+
     EXAMPLES::
 
-        sage: recherche([2,1],2)
+        sage: recherche([2,1], 2)
         True
-
-        sage: recherche([3,1],2)
+        sage: recherche([3,1], 2)
     """
     for i in liste:
         if i == val:
